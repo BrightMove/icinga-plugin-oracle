7fc8d4948471dcdfcfc1558491dac8f62066327c
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2014-01-13 21:22:12 +0100

    Initial commit

---
+++ src/main/java/net/arunoday/nagios/plugin/CheckOracle.java
@@ -0,0 +1,13 @@
+package net.arunoday.nagios.plugin;
+
+
+/**
+ * @author Aparna Chaudhary
+ */
+public class CheckOracle {
+
+	public static void main(String args[]) {
+		System.err.println("TODO");
+		// TODO:
+	}
+}
